d6ade38cc1157f267f5fbea7e6d6c0c34995f3a9
tangzx <tangzx@yikeer.com>
2016-08-16 17:46:22 +0800

    加机因图片

--- src/config/index.js
+++ src/config/index.js
@@ -2,8 +2,8 @@
  * Created by Shrimp on 16/7/31.
  */
 export default {
-//    baseUrl:'http://192.168.1.158:9003/',
     baseUrl: 'http://120.55.124.66:80/',
     photoUrl: 'http://photo.d7vg.com/avatar/',
-    imgUrl: 'http://ww4.sinaimg.cn/thumb150/'
+    imgUrl: 'http://ww4.sinaimg.cn/thumb150/',
+    flag:'http://psnine.com/Upload/flag/',//国旗
 }
--- src/layouts/GeneLayout.js
+++ src/layouts/GeneLayout.js
@@ -7,7 +7,10 @@ import {
     Text,
     StyleSheet,
     ListView,
-    RefreshControl
+    RefreshControl,
+Image,
+    Dimensions
+
 } from 'react-native'
 import {getGene} from '../services/geneService'
 import GeneItem from '../components/GeneItem'
@@ -34,7 +37,9 @@ class GeneLayout extends Component {
     }
 
     renderSeparator(sectionID, rowID, adjacentRowHighlighted) {
-        return <Separator key={sectionID+'-'+rowID}/>
+        var {height, width} = Dimensions.get('window');
+        return <Image source={require('../assets/image/ss.png')}  key={sectionID+'-'+rowID} style={{width:width}}/>
+        //return <Separator key={sectionID+'-'+rowID}/>
     }
 
     onEndReached() {
