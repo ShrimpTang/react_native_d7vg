cae8bf998a99b049ee8d89d636cf3112b543ab1d
tangzx <tangzx@yikeer.com>
2016-08-17 18:25:41 +0800

    userInfo 开始

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -43,20 +43,25 @@ const user = {
 class UserInfo extends Component {
     render() {
         var {width,height} = Dimensions.get('window');
-        var avatarUri = user.profilepicture ? user.profilepicture : config.photoUrl + user.avatar + '.png@70w.png';
+        var avatarUri = user.profilepicture ? user.profilepicture : config.photoUrl + user.avatar + '.png@100w.png';
         return (<View style={styles.container}>
             <View style={{flexDirection:'column'}}>
                 <View style={{flexDirection:'row',margin:16,marginBottom:4}}>
-                    <Image source={{uri:avatarUri}} style={{width: 45,height:45,borderRadius: 50}}/>
+                    <Image source={{uri:avatarUri}} style={{width: 100,height:100,borderRadius: 50}}/>
                     <View style={{flexDirection:'column',justifyContent:'center',marginLeft:8}}>
-                        <Text style={{color:'white',fontWeight:'bold'}}>{user.psnid}</Text>
+                        <Text style={{color:'white',fontWeight:'bold',fontSize:18}}>{user.psnid}</Text>
                         <View style={{flexDirection:'row',marginTop:5}}>
                             {this.getIcons()}
                         </View>
+                        <View style={{flexDirection:'row'}}>
+                            <Text style={{color:'#7a96d1',marginLeft:3}}>白{user.platinum}</Text>
+                            <Text style={{color:'#cd9a46',marginLeft:3}}>金{user.gold}</Text>
+                            <Text style={{color:'#a6a6a6',marginLeft:3}}>银{user.silver}</Text>
+                            <Text style={{color:'#bf6a3a',marginLeft:3}}>铜{user.bronze}</Text>
+                            <Text style={{color:'rgba(255,255,255,0.4)',marginLeft:3}}>总{parseInt(user.platinum)+parseInt(user.gold)+parseInt(user.silver)+parseInt(user.bronze)}</Text>
+                        </View>
                     </View>
-
                 </View>
-                <View style={{height:1,backgroundColor:"#BBDEFB"}}/>
             </View>
 
         </View>)
@@ -72,13 +77,19 @@ class UserInfo extends Component {
     getIcons() {
         var icons = [];
         if(user.plus==="1"){
-            icons.push(<Image key="plus" style={{width:15,height:15}} source={require('../assets/image/plus.png')}/>)
+            icons.push(<Image key="plus" style={{width:15,height:15,alignSelf:'center'}} source={require('../assets/image/plus.png')}/>)
         }
         if(user.region!==""){
-            icons.push(<Image key="flag" style={{width:15,height:15,marginLeft:10}} source={{uri:config.flagUrl+user.region+'.png'}}/>)
+            icons.push(<Image key="flag" style={{width:15,height:15,marginLeft:10,alignSelf:'center'}} source={{uri:config.flagUrl+user.region+'.png'}}/>)
         }
         if(user.level){
-            icons.push(<Image key="level" style={{width:15,height:15,marginLeft:10}} source={require('../assets/image/level.png')}/>)
+            icons.push(
+            <View key="level"  style={{flexDirection:'row',marginLeft:10}}>
+                <Image style={{width:15,height:15,alignSelf:'center'}} source={require('../assets/image/level.png')}/>
+                <Text style={{color:'#F9BD0E'}}>{user.level}</Text>
+                <Text style={{color:'white',marginLeft:6}}>{user.progress}%</Text>
+            </View>
+            )
         }
 
         return icons
